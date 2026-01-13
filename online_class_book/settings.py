16d8b9889d8703f733eb6ff871663bb88eea426f
Sneha Sonawane <snehasonawane509@gmail.com>
2026-01-13 18:31:31 +0530

    Changes settings file

--- online_class_book/settings.py
+++ online_class_book/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z^7yxr0(1j#kooy$tn-388a=b!awzbtk0&1rlmw7twur%)d@qb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 
 # Application definition
