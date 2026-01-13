3c86c59330686c5c54248ae5161a673388c388cf
Sneha Sonawane <snehasonawane509@gmail.com>
2026-01-13 19:29:28 +0530

    Changes url files

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,8 +6,15 @@ from rest_framework_simplejwt.views import (
 from .views import *
 
 urlpatterns = [
+    # Auth
+    path('login/', LoginAPI.as_view(), name='login'),
     path('register/', RegisterAPI.as_view(), name='register'),
 
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Teacher
+    path('teacher/availability/', TeacherAvailabilityAPI.as_view(), name='teacher-availability'),
+    path('teacher/bookings/', TeacherBookingsAPI.as_view(), name='teacher-bookings'),
+
+    # Student
+    path('slots/', AvailableSlotsAPI.as_view(), name='available-slots'),
+    path('book/', BookClassAPI.as_view(), name='book-class'),
 ]
